d16da3ea19ff7d680da4e0b747add28f0063bfa7
dhkehd2 <dhkehd2@gmail.com>
2024-11-25 11:31:23 +0900

    ✨ github auth 붙이기

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,10 @@
+import { auth, signIn, signOut } from "@/auth";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-function Navbar() {
+async function Navbar() {
+  const sesstion = await auth();
   return (
     <header className="px-5 py-3 bg-white shadow-sm ">
       <nav className="flex justify-between item-center">
@@ -15,7 +17,36 @@ function Navbar() {
           />
         </Link>
 
-        <div className="flex items-center gap-5"></div>
+        <div className="flex items-center gap-5 text-black">
+          {sesstion && sesstion?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut();
+                }}
+              >
+                <button type="submit">signOut</button>
+              </form>
+              <Link href={`user/${sesstion?.user?.id}`}>
+                <span>{sesstion?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
       </nav>
     </header>
   );
