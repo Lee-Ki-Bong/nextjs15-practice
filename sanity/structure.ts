f6f761b241c33571401d22a31bc2026b9bfa8b6c
dhkehd2 <dhkehd2@gmail.com>
2024-11-26 11:10:20 +0900

    🔧 author schemaType 정의
    schemaTypes/author.ts에서 타입 정의 → 스키마 로드.
    schemaTypes/index.ts에서 모든 스키마를 배열로 관리.
    structure.ts에서 사이드바에 표시될 구조 정의.

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,7 +1,7 @@
-import type {StructureResolver} from 'sanity/structure'
+import type { StructureResolver } from "sanity/structure";
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Content')
-    .items(S.documentTypeListItems())
+    .title("Content") // Sanity Studio 사이드바에 "Content"라는 제목의 그룹이 생성
+    .items([S.documentTypeListItem("author").title("Authors")]); // 그 안에 "Authors"라는 항목이 표시, 관리할 수 있도록 함
