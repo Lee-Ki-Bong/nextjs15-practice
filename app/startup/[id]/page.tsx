5119bbc114a2167ee9809188e9adccdaf6db70ab
dhkehd2 <dhkehd2@gmail.com>
2024-11-26 16:33:34 +0900

    👔 STARTUP_BY_ID_QUERY sanityFetch 추가

--- app/startup/[id]/page.tsx
+++ app/startup/[id]/page.tsx
@@ -1,7 +1,16 @@
+import { sanityFetch } from "@/sanity/lib/live";
+import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import { notFound } from "next/navigation";
 import React from "react";
 
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
+  const { data: post } = await sanityFetch({
+    query: STARTUP_BY_ID_QUERY,
+    params: { id },
+  });
+  if (!post) return notFound();
+
   return <div>page {id}</div>;
 };
 
